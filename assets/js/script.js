ba041ab42b1bff5525b0ec24da1933149ceb5345
inju69 <federico.n.diaz92@gmail.com>
2020-04-30 18:12:51 +0200

    added input and toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@
 $("#add-item").on("click", function () {
     if ($(this).prop("checked") == true) {
         console.log("Checkbox is checked.");
-        document.querySelector(".add-item__btn").classList.add("show");
+        document.querySelector(".add-item__input-text").classList.remove("hide");
     } else if ($(this).prop("checked") == false) {
         console.log("Checkbox is unchecked.");
-        document.querySelector(".add-item__btn").classList.remove("show");
+        document.querySelector(".add-item__input-text").classList.add("hide");
     }
 })
 
