70012cd51d0ca15b2699537f87e16af03d71f628
Federico Diaz <federico.n.diaz92@gmail.com>
2020-05-01 12:44:53 +0200

    input working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,45 @@
-// Globals
-$("#add-item").on("click", function () {
-    if ($(this).prop("checked") == true) {
-        console.log("Checkbox is checked.");
-        document.querySelector(".add-item__input-text").classList.remove("hide");
-    } else if ($(this).prop("checked") == false) {
-        console.log("Checkbox is unchecked.");
-        document.querySelector(".add-item__input-text").classList.add("hide");
+function addTodoId() {
+    arr = getInputsIds();
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== i) return i;
     }
-})
+
+    return arr.length;
+}
+
+function getInputsIds() {
+    let inputs = document.getElementsByTagName("input");
+    todoIds = [];
+
+    for (let inp of inputs) {
+        let curInp = inp.id.slice(0, 5);
+        if (curInp === "item-") {
+            todoIds.push(Number(inp.id.slice(5)));
+        }
+    }
+
+    todoIds.sort((a, b) => a - b);
+    return todoIds;
+}
+
+
+$(".field input").keypress(function (event) {
+    if (event.which === 13) {
+        let todoText = $(this).val();
+        $(this).val("");
+
+        let idNum = addTodoId();
+        $(".todo__list").append(`
+        <li class="todo__item">
+            <input type="checkbox" id="item-${idNum}">
+            <label for="item-${idNum}">${todoText}</label>
+        </li>`);
+
+    }
+});
+
+
 
 
 
